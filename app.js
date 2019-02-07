81bd06517e7348e9c43b68f30f0fc42e7b1f04e0
robertschaap <robert.schaap@mac.com>
2019-02-07 23:16:28 +0100

    app.js: fixed startup link

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const myport = 3000;
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.listen(myport, () => console.log(`Now listening on port ${myport}`));
+app.listen(myport, () => console.log(`Now listening on http://localhost:${myport}`));
 
 //Routes
 app.get('/', (req, res) => {
